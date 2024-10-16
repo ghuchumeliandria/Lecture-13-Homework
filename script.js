f97e898dd3b024cf2baeed10caf86b2d39fb4345
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-10-16 23:27:05 +0400

    version 2.4

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ const email = "andriaghurchumelia4@gmail.com";
 const phoneNumber = "599-91-90-49";
 
 age = true;
-
+console.info("%cSTOP", "font-size: 120px; color:red;");
 console.log(firstName);
 console.log(lastName);
 console.log(age + " %c19", "color: yellow;");
 console.log(email);
+
 // ტელეფონის ნომერი  და ასაკი ქვემოთ როგორც მაქ ეგრეც შემეძლო მარა შენ არაო თქვი :D
 // console.log(`%c ${phoneNumber}` , "color: red;")
+
 console.log("%c599-91-90-49", "color: red;");
+
 // ამის ჩაწერაც ქვემოთ როგორც მაქ შეიძლებოდა მასე მარა ლამაზი არიყო :/
 // console.log(firstName , lastName , age , email , phoneNumber);
 
+console.log("მგონი დავალება სწორად გავიგე");
+console.log("%cMafia როდის ვითამაშოთ", "color:red");
+console.log("");
 
+console.error(
+  "%cEROR 404",
+  "font-size: 120px; color:red; background-color: Black; "
+);
