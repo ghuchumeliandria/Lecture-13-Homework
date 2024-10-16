633e5e617e6c723b0aafc6b2601054cebcdcd453
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-10-16 23:19:18 +0400

    Js-start

---
+++ script.js
@@ -0,0 +1,18 @@
+const firstName = "Andria";
+const lastName = "Ghurtchumelia";
+let age = 19;
+const email = "andriaghurchumelia4@gmail.com";
+const phoneNumber = "599-91-90-49";
+
+age = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age + " %c19", "color: yellow;");
+console.log(email);
+// ტელეფონის ნომერი  და ასაკი ქვემოთ როგორც მაქ ეგრეც შემეძლო მარა შენ არაო თქვი :D
+// console.log(`%c ${phoneNumber}` , "color: red;")
+console.log("%c599-91-90-49", "color: red;");
+// ამის ჩაწერაც ქვემოთ როგორც მაქ შეიძლებოდა მასე მარა ლამაზი არიყო :/
+// console.log(firstName , lastName , age , email , phoneNumber);
+
